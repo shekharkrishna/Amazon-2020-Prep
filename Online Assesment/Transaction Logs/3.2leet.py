e2c7fa3ff87a66b04a298ff4ec512d53a0b3e8cd
Shekhar <shekharkrishnashrestha@gmail.com>
2020-11-14 05:00:51 -0500

    save placeholder for split log = log.split()

--- Online Assesment/Transaction Logs/3.2leet.py
+++ Online Assesment/Transaction Logs/3.2leet.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 def getFrauds(logs, threshold):
     data = defaultdict(int)
     for log in logs:
+        log = log.split() # This is a neat way to save an extra placeholder for split
         for user in set(log[:2]):
             data[user] += 1
     return sorted([key for key in data if data[key] >= threshold])
@@ -9,5 +10,5 @@ def getFrauds(logs, threshold):
 
 # Test
 logs = ["88 99 200", "88 99 300", "99 32 100", "12 12 15"]
-threshold = getFrauds(logs, 1)
+threshold = getFrauds(logs, 2)
 print(threshold)
