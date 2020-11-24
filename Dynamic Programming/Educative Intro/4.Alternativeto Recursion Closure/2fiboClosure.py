b518680ca28b21dfbe5843093bfcee0635d87260
Shekhar <shekharkrishnashrestha@gmail.com>
2020-11-23 19:55:54 -0500

    Seperation of logic and power in Closure

--- Dynamic Programming/Educative Intro/4.Alternativeto Recursion Closure/2fiboClosure.py
+++ Dynamic Programming/Educative Intro/4.Alternativeto Recursion Closure/2fiboClosure.py
@@ -12,13 +12,25 @@ def fibo():
 
 
 # Test
-print('Fibo(1): ', fibo())
+print('''Outer Function is just as object that holds the inner function
+ and the local values that might be helpful for dynamism \n''', fibo())
+
+f = fibo()
+print('''\n Default Logic will compute the smallest fib number. 
+        \t Inner Function Logic is for Fib of 1: ''', f())
+
+
+# Fib of n can only be achieved using the loop till n.
 fibonacci = fibo()
 
 # To get the Fibo of i we can loop in
 for i in range (20):
     # num = fibonacci() # waist of placeholder
     print(f'Fib of {i+1} is: {fibonacci()}')
+
+# this loop above is just like an engine for power to repetatively preform the 
+# main logic that is defined in inner function of outer function. 
+print('Seperation of logic and power is cool')
 
 
 
